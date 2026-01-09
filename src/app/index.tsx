1deb730fe930cbb577a0b0cc79f139f02b67631b
notd3nnis <dennisjunior247@gmail.com>
2026-01-09 17:07:33 +0000

    deleted defualt folder strutrue to create custom one

---
+++ src/app/index.tsx
@@ -0,0 +1,15 @@
+import { Image } from 'expo-image';
+import { Platform, StyleSheet,View } from 'react-native';
+
+
+import { Link } from 'expo-router';
+
+export default function HomeScreen() {
+  return (
+   <View >
+    Testing
+   </View>
+  );
+}
+
+
