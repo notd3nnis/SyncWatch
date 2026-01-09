2c872ae687cfad047ffc2bbb3e83d2676f55eecd
notd3nnis <dennisjunior247@gmail.com>
2026-01-09 18:27:36 +0000

    worked on the splash screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,12 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet,View } from 'react-native';
+import {View,Text  } from 'react-native';
 
-
-import { Link } from 'expo-router';
 
 export default function HomeScreen() {
   return (
    <View >
+    <Text>
     Testing
+    </Text>
    </View>
   );
 }
