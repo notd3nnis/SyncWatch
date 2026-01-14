8618f8b2f78334ee2d3074db5e4dc0aeff737d61
notd3nnis <dennisjunior247@gmail.com>
2026-01-14 02:26:53 +0000

    basic theming

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,14 @@
-import { TamaguiProvider, View, Text, Button } from "tamagui";
+import { TamaguiProvider, View, Button } from "tamagui";
 import tamaguiConfig from "../../tamagui.config"; // Your config
 
 export default function App() {
   return (
     <TamaguiProvider config={tamaguiConfig}>
       <View>
-        <Button size="50">
-          Testing
+        <Button size="20">
+          Testing the new button 
         </Button>
       </View>
     </TamaguiProvider>
-  );
+  )
 }
--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -1,78 +1,112 @@
-import { Platform } from "react-native";
+import { createTokens, createTamagui, styled, Text } from "tamagui";
 
-export const Colors = {
-  neutral: {
-    neutral01: "#0A0A0A",
-    netural02: "#262626",
-    netural03: "#262626",
-    netural04: "#404040",
-    netural05: "#A3A3A3",
-    netural06: "#D4D4D4",
-    netural07: "#E5E5E5",
-    netural08: "#E5E5E5",
-    netural09: "#F5F5F5",
-    netural10: "#FAFAFA",
-  },
-  green: {
-    green01: "#026B3F",
-    green02: "#00A14B",
-    green03: "#00C059",
-    green04: "#70E2A0",
-    green05: "#D3F6E4",
-  },
-  red: {
-    red01: "#8C1C13",
-    red02: "#D83037",
-    red03: "#FF3B30",
-    red04: "#E63946",
-    red05: "#F8D7DA",
-  },
-  yellow: {
-    yellow01: "#946F00",
-    yellow02: "#E6A800",
-    yellow03: "#FFBE00",
-    yellow04: "#FFD966",
-    yellow05: "#FFF4CC",
-  },
+// PALETTE
+const palette = {
+  pinkPrimary: "#FF007B",
+  purplePrimary: "#7130F0",
+  yellowPrimary: "#FEC500",
+
+  black: "#000000",
+  white: "#FFFFFF",
 };
 
-export const Typography = {
-  fontSizes: {
-    extraBold: "32px",
-    bold: "24px",
-    medium: "20px",
-    regular: "16px",
+// TOKENS
+const tokens = createTokens({
+  color: palette,
+  space: {
+    s: 8,
+    m: 16,
+    l: 24,
+    xl: 40,
   },
-  fontweight: {
-    extrabold: "800",
-    bold: "700",
-    semiBold: "600",
-    medium: "500",
-    regular: "400",
+  size: {
+    s: 8,
+    m: 16,
+    l: 24,
+    xl: 40,
   },
-  lineHeights: {
-    extraBold: "40px",
-    bold: "32px",
-    medium: "28px",
-    regular: "24px",
-  },
-};
-
-export const Fonts = Platform.select({
-  ios: {
-    /** iOS `UIFontDescriptorSystemDesignDefault` */
-    sans: "system-ui",
-    /** iOS `UIFontDescriptorSystemDesignSerif` */
-    serif: "ui-serif",
-    /** iOS `UIFontDescriptorSystemDesignRounded` */
-    rounded: "ui-rounded",
-    /** iOS `UIFontDescriptorSystemDesignMonospaced` */
-    mono: "ui-monospace",
-  },
-  default: {
-    sans: "normal",
-    serif: "serif",
-    rounded: "normal",
-    mono: "monospace",
+  radius: {
+    0: 0,
+    1: 4,
+    2: 8,
+    3: 12,
+    4: 16,
   },
 });
+
+// THEME
+const darkTheme = {
+  background: palette.black,
+  primary: palette.pinkPrimary,
+  secondary: palette.purplePrimary,
+  accent: palette.yellowPrimary,
+};
+
+// TAMAGUI CONFIG
+const config = createTamagui({
+  tokens,
+  themes: {
+    dark: darkTheme,
+  },
+});
+
+type AppConfig = typeof config;
+declare module "tamagui" {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  interface TamaguiCustomConfig extends AppConfig {}
+}
+
+// TYPOGRAPHY COMPONENTS
+
+export const Typography = styled(Text, {
+  color: "$color",
+
+  variants: {
+    variant: {
+      h1: {
+        fontSize: 28,
+        lineHeight: 34,
+      },
+      h2: {
+        fontSize: 20,
+        lineHeight: 25,
+      },
+      subheading: {
+        fontSize: 17,
+        lineHeight: 22,
+      },
+      body: {
+        fontSize: 15,
+        lineHeight: 20,
+      },
+      small: {
+        fontSize: 13,
+        lineHeight: 18,
+      },
+      smaller: {
+        fontSize: 12,
+        lineHeight: 16,
+      },
+      caption: {
+        fontSize: 11,
+        lineHeight: 13,
+      },
+    },
+
+    weight: {
+      regular: { fontWeight: "400" },
+      medium: { fontWeight: "500" },
+      semibold: { fontWeight: "600" },
+      bold: { fontWeight: "700" },
+      extrabold: { fontWeight: "800" },
+    },
+  } as const,
+
+  defaultVariants: {
+    variant: "body",
+    weight: "regular",
+  },
+});
+
+export default config;
+export { palette, tokens, darkTheme };
