5c44320bab81a6035c59c00e738afdeecdd14a8a
notd3nnis <dennisjunior247@gmail.com>
2026-02-05 12:11:43 +0000

    stating settings page

--- src/utils/dummyData.tsx
+++ src/utils/dummyData.tsx
@@ -1,5 +1,10 @@
 import { SlideData } from "../components/OnboardingSlide/types";
-import { NetflixLogo, OnboardingImgOne, PrimeLogo } from "../assets/svgs";
+import {
+  NetflixLogo,
+  OnboardingImgOne,
+  OnboardingImgTwo,
+  PrimeLogo,
+} from "../assets/svgs";
 import { selectProviderType } from "./types";
 import { MovieProps } from "../screens/HomePage/CreateParty/types";
 
@@ -15,7 +20,7 @@ export const onboardingData: SlideData[] = [
     id: 2,
     title: "One tap. Everyone's in!",
     description: "Start the movie, chat live, & react in real time.",
-    image: <OnboardingImgOne />,
+    image: <OnboardingImgTwo />,
   },
 ];
 
@@ -101,7 +106,6 @@ export const moviesData: MovieProps[] = [
     description: "An intense drama exploring complex themes.",
   },
 ];
-
 
 export const CurrentParties = [
   {
